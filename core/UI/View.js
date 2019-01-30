59edd181de62b6e5264c128218842df0246694d2
Max Orlov <v6825403@hotmail.com>
2019-01-30 07:32:55 +0000

    added View object

--- core/UI/View.js
+++ core/UI/View.js
@@ -1,5 +1,24 @@
+/* global Tools */
 "use strict";
 
-function View() {
+function View(options, params) {
+    
+    if(!options || !options.name) {
+        throw new Error("View must have options atleast with the 'name' property!");
+    }
+    
+    const _private = {};
+    _private.callbackOnClose = undefined;
+    _private.callbackOnShow  = undefined;
+    
+    Object.defineProperty(this, "id", { value: Tools.SID(), enumerable: false, writable: false });
+    Object.defineProperty(this, "name", { value: options.name, enumerable: false, writable: false });
+    
+    this.params = params;
+    if(params.instance) {
+        this.instance = params.instance;
+        this.instance.view = this;
+    }
+    
     
 }
