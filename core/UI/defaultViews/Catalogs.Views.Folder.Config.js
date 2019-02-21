03d1319370bbe97f2468672f35c91612292695cd
proudbird <v6825403@hotmail.com>
2019-02-21 07:03:21 +0100

    Lookup implementation

--- core/UI/defaultViews/Catalogs.Views.Folder.Config.js
+++ core/UI/defaultViews/Catalogs.Views.Folder.Config.js
@@ -13,17 +13,17 @@ module.exports.Init = function (item) {
             "booked", "Date", "parentId", "ownerId", "order"];
 
   if(definition.owners && definition.owners.length){
-    attributes.push({ id: "Owner", header: "Owner", type: "FK" });
+    attributes.push({ id: "Owner", header: "Owner", type: "FK", link: "Owner" });
   }
 
   if(definition.multilevel){
-    attributes.push({ id: "Parent", header: "Parent", type: "FK" });
+    attributes.push({ id: "Parent", header: "Parent", type: "FK", link: "Parent", onlyFolders: true });
   }
 
   if(definition.codeLenght > 0){
     attributes.push({ id: "Code", header: "Code" });
   }
-
+  
   attributes.push({ id: "Name", header: "Name" });
 
   for (let key in definition.attributes) {
@@ -42,12 +42,19 @@ module.exports.Init = function (item) {
   const rows = [{ view: "Toolbar", name: "Toolbar", owner: viewName, composition: "default", elements: [] }];
 
   attributes.forEach(row => {
+    let viewType;
+    if(row.type === "FK") {
+      viewType = "Lookup";
+    } else {
+      viewType = "Text";
+    }
     rows.push(
       {
-        view: "Text", 
+        view: viewType, 
         name: row.id,
         label: row.header,
-        dataLink: "item." + row.id
+        dataLink: "item." + row.id,
+        onlyFolders: row.onlyFolders
       }
     );
   })
