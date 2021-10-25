253b01da7dc48a753c2a01519a32d7600e39d60b
proudbird <v6825403@hotmail.com>
2021-10-25 14:02:11 +0200

    clean

--- start.ts
+++ start.ts
@@ -1,15 +1,9 @@
 import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs'
 import path    from 'path'
 import http    from 'http'
-import SysTray from 'systray'
-import notifier from 'node-notifier'
 import Cubismo from './cubismo/Cubismo'
 import './common/Utils'
 import dotenv from "dotenv";
-
-
-import puppeteer from 'puppeteer'
-import { xor } from 'lodash'
 
 dotenv.config();
 
@@ -17,6 +11,7 @@ let cubismo: Cubismo
 let sekretKey: string
 let applicationId: string
 let port: number = 21021
+let host: string = '127.0.0.1';
 let stopPort: number
 let message: string
 
@@ -46,14 +41,6 @@ async function runServer() {
   cubismo = new Cubismo(__dirname, sekretKey);
   await cubismo.start(applicationId)
   message = `port ${port}`
-  // notifier.notify({
-  //   title: 'Server is started',
-  //   message: message,
-  //   icon: path.join(__dirname, 'favicon.png'),
-  //   sound: true,
-  //   wait: false,
-  //   appName: 'cubismo'
-  // })
 }
 
 async function stopServer(stopPort, restart?: boolean) {
@@ -63,11 +50,6 @@ async function stopServer(stopPort, restart?: boolean) {
   if(!restart) {
   // check if there is already running instance of cubismo server
     if(await checkInstance(stopPort)) {
-
-      function exit() {
-        //systray.kill()
-        process.exit()
-      }
     
       const result = await stopInstance(stopPort, sekretKey) 
       if(result.error) {
@@ -75,18 +57,7 @@ async function stopServer(stopPort, restart?: boolean) {
       }
       message = `cubismo server is stopped`
       Logger.info(message)
-      notifier.notify({
-        title: 'Server is stopped',
-        message: '...',
-        icon: path.join(__dirname, 'favicon.png'),
-        sound: true,
-        appName: 'cubismo'
-      },
-      function() {
-        // to show notification
-        setTimeout(exit, 500)
-      })
-      
+      process.exit()
     }
   } else {
     // destroing cubismo instance
@@ -192,102 +163,3 @@ export function initKey(): string {
 
   return key
 }
-
-// SYSTRAY
-// const systray = new SysTray({
-//   menu: {
-//     // you should using .png icon in macOS/Linux, but .ico format in windows
-//     icon: readFileSync('favicon.ico','base64'),
-//     title: "cubismo",
-//     tooltip: "cubismo server",
-//     items: [
-//       {
-//         title: `Running on port ${port}`,
-//         tooltip: undefined,
-//         checked: false,
-//         enabled: true
-//       },
-//       {
-//         title: "Restart server",
-//         tooltip: "Restart server",
-//         // checked is implement by plain text in linux
-//         checked: false,
-//         enabled: true
-//       },
-//       {
-//         title: "Stop server",
-//         tooltip: "Stop server",
-//         // checked is implement by plain text in linux
-//         checked: false,
-//         enabled: true
-//       }
-//     ]
-//   },
-//   debug: false,
-//   copyDir: true, // copy go tray binary to outside directory, useful for packing tool like pkg.
-// })
-
-// systray.onClick(action => {
-//   if(action.seq_id === 1) {
-//     stopServer(port, true)
-//   } else if(action.seq_id === 2) {
-//     stopServer(port)
-//   }
-// })
-
-// setTimeout(toPdf, 20);
-
-// async function toPdf() {
-
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto("http://localhost:21021/app/optima");
-//   console.log('111')
-//   const elements = await page.$$('[label]');
-//   console.log('222')
-//   if(elements.length) {
-//     console.log('333')
-//     let count = 1;
-//     let x = 0, y = 0;
-//     let width = 219.2;
-//     let height = 152.17;
-//     for(let count = 0; count<elements.length; count++) {
-//       console.log('444')
-//       const el = elements[count];
-//       console.log('555')
-//       // try{
-//       //   await page.screenshot({ path: path.join(__dirname, `img${count}.png`),
-//       //     clip: {
-//       //       x,
-//       //       y,
-//       //       width,
-//       //       height
-//       //     }
-//       //   });
-//       //   y+=height;
-//       // } catch(error) {
-//       //   console.log(`can't make screenshot ${error.message}`);
-//       // }  
-      
-//       try{
-//         await el.screenshot({ path: path.join(__dirname, `img${count}.png`) });
-//         y+=height;
-//       } catch(error) {
-//         console.log(`can't make screenshot ${error.message}`);
-//       }  
-
-//     }
-//   }
-
-//   // await page.pdf({
-//   //   path: path.join(__dirname, 'test2.pdf'), 
-//   //   //scale: 1.465,
-//   //   width: '58.2mm',
-//   //   height: '40.1mm',
-//   //   margin: {
-//   //     top: 0, right: 0, bottom: 0, left: 0
-//   //   }
-//   // });
-
-//   await browser.close();  
-// }
