91e1ec6878e9c560f290eaebfb62b663c89a4c40
proudbird <v6825403@hotmail.com>
2022-07-01 17:24:52 +0200

    feat: add index app script

---
+++ addIndex.js
@@ -0,0 +1,61 @@
+import Cubismo from "/core/Cubismo";
+
+const configFile = process.argv[2];
+const configPath = path.join(__dirname, configFile);
+if (!fs.existsSync(configPath)) {
+  console.error(`Can't find config file ${configPath}`);
+  process.exit(0);
+}
+
+let content;
+let config;
+
+try {
+  content = fs.readFileSync(configPath, 'utf-8');
+} catch (error) {
+  console.error(`Can't read config file ${configFile}`);
+  process.exit(0);
+}
+
+try {
+  config = JSON.parse(content);
+} catch (error) {
+  console.error(`Can't parse config file ${configFile}`);
+  process.exit(0);
+}
+
+const paramsFile = process.argv[3];
+const paramsPath = path.join(__dirname, paramsFile);
+if (!fs.existsSync(configPath)) {
+  console.error(`Can't find params file ${paramsPath}`);
+  process.exit(0);
+}
+
+let content;
+let params;
+
+try {
+  content = fs.readFileSync(paramsPath, 'utf-8');
+} catch (error) {
+  console.error(`Can't read params file ${paramsFile}`);
+  process.exit(0);
+}
+
+try {
+  params = JSON.parse(content);
+} catch (error) {
+  console.error(`Can't parse params file ${paramsFile}`);
+  process.exit(0);
+}
+
+async function execute() {
+  try {
+    const cubismo = new Cubismo(config);
+    await cubismo.start();
+    cubismo.addApplication(params);
+  } catch (error) {
+    throw new Error(`Can't init Index app: ${error}`);
+  }
+}
+
+execute();
